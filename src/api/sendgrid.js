6b01065a459bdef89114bf66435003bf16c9ceff
Koki Shibata <kenergyintense@gmail.com>
2021-08-19 11:17:29 +0900

    add: send grid function

---
+++ src/api/sendgrid.js
@@ -0,0 +1,46 @@
+import sendgrid from '@sendgrid/mail';
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+const message = {
+  from: process.env.SENDGRID_AUTHORIZED_EMAIL,
+};
+
+const handler = (req, res) => {
+  try {
+    if (req.method !== 'POST') {
+      res.json({ message: 'try a POST!' });
+    }
+
+    if (req.body) {
+      message.to = req.body.email;
+      message.subject = req.body.subject;
+      message.text = req.body.text;
+      message.html = req.body.text;
+    }
+
+    return sendgrid.send(message).then(
+      () => {
+        res.status(200).json({
+          message: 'success! I will send email',
+        });
+      },
+      // eslint-disable-next-line
+      (err) => {
+        console.error(err);
+        if (err.response) {
+          return res.status(500).json({
+            error: err.response,
+          });
+        }
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: err,
+      message: 'there was an error, try again',
+    });
+  }
+};
+
+module.exports = handler;
